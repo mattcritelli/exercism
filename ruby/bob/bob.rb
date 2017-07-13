91a92096f5fe28a691345bc03021e9fc3eadc34a
mattcritelli <matthew.critelli@gmail.com>
2017-07-13 13:51:38 -0600

    Need to fix 4 remaining failing tests.

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -5,23 +5,33 @@ class Bob
   def self.hey(remark)
     @remark = remark
     if say_nothing
+      p "say_nothing"
       "Fine. Be that way!"
     elsif question? && !all_caps
+      p "question? && !all_caps"
       "Sure."
     elsif question? && all_caps
+      p "question? && all_caps"
       "Whoa, chill out!"
     elsif question?
+      p "question?"
       "Sure."
-    elsif all_caps
+    elsif all_caps #&& include_letters
+      p "all_caps"
       "Whoa, chill out!"
     else
+      p "else"
       "Whatever."
     end
   end
 
+  # def self.include_letters
+  #   alphabet = [*('a'..'z')]
+  #   @remark.split.any? { |char| alphabet.include?(char)}
+  # end
+
   def self.all_caps
     @remark.gsub(/\W/,"").split("").all? { |letter| letter == letter.upcase }
-    # binding.pry
   end
 
   def self.question?
